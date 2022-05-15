9d3558cdc3191b6a2059913f2fcda3801a56c01b
Jorge Flores <jorge.flores.m@tecsup.edu.pe>
2022-05-15 14:55:57 -0500

    lab09

---
+++ database/migrations/2022_05_15_001947_create_registro_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registro', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('nickname');
+            $table->string('genero');
+            $table->date('fecha_nacimiento');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registro');
+    }
+}
